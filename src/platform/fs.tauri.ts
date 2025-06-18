cf9f34e4af0d16b86e8026ae3a3acb1c4b822636
JumpyJacko <bobjacklee27@gmail.com>
2025-06-18 11:44:49 +1000

    fix(fs): tauri is stupid and i have to get children paths manually

--- src/platform/fs.tauri.ts
+++ src/platform/fs.tauri.ts
@@ -2,7 +2,7 @@ import { open } from '@tauri-apps/plugin-dialog';
 import { DirEntry, readDir } from '@tauri-apps/plugin-fs';
 import { Series } from '../models/Series';
 import { Chapter } from '../models/Chapter';
-import { volumeRegex } from './fs';
+import { chapterRegex, volumeRegex } from './fs';
 
 export async function requestLibraryFolderAccess(): Promise<Series[] | undefined> {
     const path: string | null = await open({
@@ -13,23 +13,44 @@ export async function requestLibraryFolderAccess(): Promise<Series[] | undefined
     if (!path) {
         return;
     }
+    
+    const dirEntries = await readDir(path);
 
-    const dirHandle = await readDir(path);
+    for (const e of dirEntries) {
+        if (e.isDirectory) {
+            // NOTE: wary of windows?
+            const seriesPath = path + "/" + e.name;
+            const seriesEntries = await readDir(seriesPath);
+            const series: Series = await constructSeries(seriesPath, seriesEntries);
+        }
+    }
 }
 
-async function constructSeries(handle: DirEntry[]): Promise<Series> {
-    if (!handle) {
-        throw new Error("No handle provided");
+async function constructSeries(path: string, entries: DirEntry[]): Promise<Series> {
+    if (!entries) {
+        throw new Error("No handle provided.");
+    }
+    if (!path) {
+        throw new Error("No path provided.");
     }
 
     const orphanedChapterList: Chapter[] = [];
     const volumes: Volume[] = [];
 
-    for (const h of handle) {
-        if (h.isDirectory) {
-            if (volumeRegex.test(h.name)) {
-                const series: Series = await constructSeries(h);
+    for (const e of entries) {
+        console.log(e);
+        if (e.isDirectory) {
+            if (volumeRegex.test(e.name)) {
+                // TODO: do volume stuff
+            } else if (chapterRegex.test(e.name)) {
+                const chapterPath = path + "/" + e.name;
+                const chapterEntries = await readDir(chapterPath);
+                const chapter: Chapter = await constructChapter(chapterPath, chapterEntries);
+                
             }
         }
     }
+}
+
+async function constructVolume(path: string, entries: DirEntry[]) {
 }
