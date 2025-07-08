e157142b0e4b7b26b40619d72aea961b1fa3c18e
JumpyJacko <bobjacklee27@gmail.com>
2025-07-08 12:00:20 +1000

    add: convert anilist status to plain english

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,3 +10,20 @@ export function shuffleArray<T>(array: T[]) {
             array[randomIndex], array[currentIndex]];
     }
 }
+
+export function convertAniListStatus(status: string): string {
+    switch (status) {
+        case "FINISHED":
+            return "Publishing Finished";
+        case "RELEASING":
+            return "Currently Publishing";
+        case "CANCELLED":
+            return "Cancelled";
+        case "HIATUS":
+            return "On Hiatus";
+        case "NOT_YET_RELEASED":
+            return "Not Released";
+        default:
+            return "?";
+    }
+}
