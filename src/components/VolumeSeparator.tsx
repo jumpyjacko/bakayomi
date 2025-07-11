9b3ca9e51e155de36e2e0b1db914c32c647fa300
JumpyJacko <bobjacklee27@gmail.com>
2025-07-11 13:43:46 +1000

    fix: dim volume separator

--- src/components/VolumeSeparator.tsx
+++ src/components/VolumeSeparator.tsx
@@ -2,11 +2,11 @@ import i18next from "i18next";
 
 export default function VolumeSeparator(props) {
     return (
-        <div class="w-full p-2 my-1 typo-body text-shadow-md flex flex-row items-center">
-        <p class="pr-2">
-        {props.title === "No Volume" ? i18next.t("series_v:noSpecifiedVolume") : props.title}
-        </p>
-        <hr class="flex-1" />
+        <div class="w-full p-2 my-1 typo-body text-shadow-md flex flex-row items-center text-text/30">
+            <p class="pr-2">
+            {props.title === "No Volume" ? i18next.t("series_v:noSpecifiedVolume") : props.title}
+            </p>
+            <hr class="flex-1" />
         </div>
     );
 }
