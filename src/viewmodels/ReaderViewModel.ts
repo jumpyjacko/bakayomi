0864092a1b2b5be2ea9711d584377f696fdd6c5e
JumpyJacko <bobjacklee27@gmail.com>
2025-07-17 09:10:30 +1000

    add: go to next chapter at end of current chapter

--- src/viewmodels/ReaderViewModel.ts
+++ src/viewmodels/ReaderViewModel.ts
@@ -1,11 +1,12 @@
 import { createEffect, createSignal, onMount } from "solid-js";
-import { useParams } from "@solidjs/router";
+import { useNavigate, useParams } from "@solidjs/router";
 
 import { isTauri } from "@tauri-apps/api/core";
 
-import { Volume } from "../models/Volume";
 import { Library } from "../models/Library";
 import { Series } from "../models/Series";
+import { Volume } from "../models/Volume";
+import { Chapter } from "../models/Chapter";
 import { Page } from "../models/Page";
 
 import { getChapterPages, verifyPermission } from "../platform/fs";
@@ -13,10 +14,18 @@ import { getItem } from "../db/db";
 
 export function createReaderViewModel() {
     const params = useParams();
+    const navigate = useNavigate();
 
     const [pageList, setPageList] = createSignal<Page[]>([]);
     const [pageNumber, setPageNumber] = createSignal(0);
     const [currentPage, setCurrentPage] = createSignal<Page>();
+
+    let volumeIdx: number;
+    let chapterIdx: number;
+
+    let series: Series | undefined;
+    let volume: Volume;
+    let chapter: Chapter;
 
     onMount(async () => {
         const tauri = isTauri();
@@ -30,9 +39,11 @@ export function createReaderViewModel() {
         const volumeName = decodeURIComponent(params.volume);
         const chapterName = decodeURIComponent(params.chapter);
 
-        const series = await getItem<Series>("local_library", seriesName);
-        const volume = series?.volumes?.find((v) => v.title === volumeName);
-        const chapter = volume?.chapters.find((c) => c.title === chapterName);
+        series = await getItem<Series>("local_library", seriesName); // TODO: Handle no series found (i.e. redirect to 404)
+        volumeIdx = series!.volumes!.findIndex((v) => v.title === volumeName);
+        volume = series!.volumes![volumeIdx];
+        chapterIdx = volume.chapters.findIndex((c) => c.title === chapterName);
+        chapter = volume.chapters[chapterIdx];
 
         const pages: Page[] = await getChapterPages(chapter?.handle);
         pages.sort((a, b) => a.name.localeCompare(b.name));
@@ -47,7 +58,31 @@ export function createReaderViewModel() {
         setCurrentPage(list[num]);
     });
 
+    async function nextChapter() {
+        if (chapterIdx < volume.chapter_count - 1) {
+            chapterIdx++;
+        } else if (volumeIdx < series!.volumes!.length - 1) {
+            volumeIdx++;
+            chapterIdx = 0;
+        } else {
+            // TODO: show a "No more volumes or chapters" screen
+            return;
+        }
+        
+        volume = series!.volumes![volumeIdx];
+        chapter = volume.chapters[chapterIdx];
+
+        navigate(`/read/${series!.title}/${volume.title}/${chapter.title}`);
+        const pages: Page[] = await getChapterPages(chapter?.handle);
+        setPageList(pages);
+        setCurrentPage(pageList()[pageNumber()]);
+    }
+
     function nextPage() {
+        if (pageNumber() === pageList().length - 1) {
+            nextChapter();
+        }
+        
         setPageNumber((last) => Math.min(last + 1, pageList().length - 1));
     }
 
